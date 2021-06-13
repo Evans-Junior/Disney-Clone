68d5393eff170ecadc0c4b8ceaf117f2c7a24011
master <kwakukumi14@gmail.com>
2021-06-13 22:05:26 +0000

    work in progress

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+import Detail from './components/Detail';
+import Login from './components/Login';
 
 
 function App() {
-  return (
+  return(
+    <Router>
     <div className="App">
+    
         <Header/>
-        <Home/>
+        <Switch>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route exact path='/'>
+            <Home/>
+            </Route>
+            <Route path='/detail'>
+             <Detail/>
+            </Route>
+
+        </Switch>
+      
     </div>
+    </Router>
   );
 }
 
